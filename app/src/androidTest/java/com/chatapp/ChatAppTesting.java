8f8405b848522e6204073d4e1d28cd59a5b79768
Usman Khalid <usmankhan495@gmail.com>
2018-03-31 13:11:37 +0500

    chat app commit

--- app/src/androidTest/java/com/chatapp/ChatAppTesting.java
+++ app/src/androidTest/java/com/chatapp/ChatAppTesting.java
@@ -8,13 +8,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthInvalidUserException;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Logger;
-
-import junit.framework.AssertionFailedError;
 
 import org.junit.Test;
 
@@ -26,33 +20,13 @@ import java.util.concurrent.TimeUnit;
  */
 
 public class ChatAppTesting extends AndroidTestCase {
-    //private static Logger logger = LoggerFactory.getLogger(ChatAppTesting.class);
 
     private CountDownLatch authSignal = null;
     private FirebaseAuth auth;
 
     @Override
     public void setUp() throws InterruptedException {
-       /* authSignal = new CountDownLatch(1);
-       // Firebase.setAndroidContext(mContext); //initializeFireBase(context);
 
-        auth = FirebaseAuth.getInstance();
-        if(auth.getCurrentUser() == null) {
-            auth.signInWithEmailAndPassword("ali@gmail.com", "12345678").addOnCompleteListener(
-                    new OnCompleteListener<AuthResult>() {
-
-                        @Override
-                        public void onComplete(@NonNull final Task<AuthResult> task) {
-
-                            final AuthResult result = task.getResult();
-                            final FirebaseUser user = result.getUser();
-                            authSignal.countDown();
-                        }
-                    });
-        } else {
-            authSignal.countDown();
-        }
-        authSignal.await(10, TimeUnit.SECONDS);*/
     }
 
     @Override
@@ -66,7 +40,6 @@ public class ChatAppTesting extends AndroidTestCase {
     @Test
     public void testLogin() throws InterruptedException{
         authSignal = new CountDownLatch(1);
-        // Firebase.setAndroidContext(mContext); //initializeFireBase(context);
 
         auth = FirebaseAuth.getInstance();
         if(auth.getCurrentUser() == null) {
@@ -78,7 +51,6 @@ public class ChatAppTesting extends AndroidTestCase {
 
                             final AuthResult result = task.getResult();
                             final FirebaseUser user = result.getUser();
-                               //assertEquals(null,user);
                             authSignal.countDown();
 
 
@@ -93,7 +65,6 @@ public class ChatAppTesting extends AndroidTestCase {
     @Test
     public void testSignUp() throws InterruptedException{
         authSignal = new CountDownLatch(1);
-
         auth = FirebaseAuth.getInstance();
         if(auth.getCurrentUser() == null) {
             auth.createUserWithEmailAndPassword("usmankhalid@gmail.com", "12345678").addOnCompleteListener(
@@ -101,7 +72,6 @@ public class ChatAppTesting extends AndroidTestCase {
 
                         @Override
                         public void onComplete(@NonNull final Task<AuthResult> task) {
-
                             final AuthResult result = task.getResult();
                             final FirebaseUser user = result.getUser();
 
